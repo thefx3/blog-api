f18bb72828cdbbf51b3324557d79e5b888db864a
thefx3 <fx.pairault@yahoo.fr>
2025-11-19 14:15:51 +0100

    Init server.js

---
+++ server/server.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+console.log("Environment loaded:", process.env.DB_HOST, process.env.DB_DATABASE);
+
+const path = require("node:path");
+const express = require('express');
+const cors = require('cors');
+
+
+// Initialize express app
+const app = express();
+
+//-------------- MIDDLEWARES --------------------------
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+// -------------- SERVER --------------------------------
+const PORT = process.env.PORT || 8080;
+(async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`✅ App listening on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("❌ Database initialization failed:", err);
+  }
+})();
+
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
