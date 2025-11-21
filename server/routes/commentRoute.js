96ec4651ca2a03b0879501f664fd6b815da04faa
thefx3 <fx.pairault@yahoo.fr>
2025-11-21 12:19:25 +0100

    commentController testing done

--- server/routes/commentRoute.js
+++ server/routes/commentRoute.js
@@ -2,16 +2,17 @@ const { Router } = require("express");
 const router = Router();
 
 const commentController = require('../controllers/commentController');
+const authRequired = require("../auth/auth");
 
 // Already from : "/api/posts/..."
 
 router.get('/post/:postId', commentController.listCommentsByPost);
 
-router.post("/post/:postId", commentController.createComment);
+router.post("/post/:postId", authRequired, commentController.createComment);
 
-router.put("/:id", commentController.updateComment);
+router.put("/:id",authRequired, commentController.updateComment);
 
-router.delete("/:id", commentController.deleteComment);
+router.delete("/:id", authRequired, commentController.deleteComment);
 
 
 module.exports = router;
